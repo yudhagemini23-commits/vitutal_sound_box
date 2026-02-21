15ac9b19227db65a90484c93c04d1d326dd8c2ca
Yudha <yudha.pp23@gmail.com>
2026-02-21 12:09:20 +0700

    tester notification

--- app/src/main/java/com/app/virtualsoundbox/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/app/virtualsoundbox/ui/dashboard/DashboardScreen.kt
@@ -24,15 +24,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.app.virtualsoundbox.data.local.AppDatabase
